f40604d28dfcf03211c9f25e318a83e411c0ef76
Mhd Ramadhan Arvin <mramadhan687@gmail.com>
2022-09-23 11:48:16 +0700

    Progress

--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,8 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+// $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('.*Bonjour.*', function ($bot) {
+    $bot->reply('Nice to meet you!');
+});
